ebd71e37f5be2a5ba226e00370feea0a88fc2554
shazaniyu55 <shazaniyu@gmail.com>
2025-03-10 05:32:23 +0100

    new-link

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://localhost:8080");
+const socket = new WebSocket("wss:https://webcam-lac.vercel.app");
 let localStream, peerConnection, userId, remoteUserId;
 const config = { iceServers: [{ urls: "stun:stun.l.google.com:19302" }] };
 
